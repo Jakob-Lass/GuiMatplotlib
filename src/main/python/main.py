479b8c347bebe343efed8cfef53dcf6b1cbe8787
Jakob Lass <lass.jakob@gmail.com>
2020-07-12 18:39:19 +0200

    Customize main window load

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,12 +1,30 @@
-from fbs_runtime.application_context.PyQt5 import ApplicationContext
-from PyQt5.QtWidgets import QMainWindow
+from fbs_runtime.application_context.PyQt5 import ApplicationContext, \
+    cached_property
 
+from MainWindow import MainWindow
 import sys
 
-if __name__ == '__main__':
-    appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    window = QMainWindow()
-    window.resize(250, 150)
-    window.show()
-    exit_code = appctxt.app.exec_()      # 2. Invoke appctxt.app.exec_()
+class AppContext(ApplicationContext):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+        
+
+    def run(self):
+        self.main_window.show()
+        
+        return self.app.exec_()
+
+    @cached_property
+    def main_window(self):
+        res = MainWindow()
+        
+        return res # Pass context to the window.
+
+def main():
+    appctxt = AppContext()
+    exit_code = appctxt.run()
     sys.exit(exit_code)
+
+if __name__ == '__main__':
+    main()
